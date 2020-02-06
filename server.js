340b53139f97c8247757dd23053d39d3ee3460d8
Wismy <53704939+wseide@users.noreply.github.com>
2020-02-06 13:53:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
-//Techonology needed to begin and start app
 const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
-const routes = require("./routes")
+const PORT = process.env.PORT || 3001;
 
-
-// Define middleware here
+// Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-//For connecting MongoDB
-const mongoose = require("mongoose");
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactgooglebooks");
-
-
-// Serve up static assets (usually on heroku)
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
@@ -22,9 +16,17 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-const PORT = process.env.PORT || 3001;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
+
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
 
